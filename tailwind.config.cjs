751c6d766cce22ddaf3aa21c3608d24583c2627c
ben10pedrin <pedro123ben10@gmail.com>
2023-05-26 17:08:59 -0600

    add expert q-a carrousel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      maxWidth: {
-        xxs: "240px",
-      },
       colors: {
         beige: {
           300: "#F5F5EB",
