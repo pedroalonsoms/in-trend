540f2a5e5571c34929e2623f0b2d689a0d1fc4e4
ben10pedrin <pedro123ben10@gmail.com>
2023-06-01 17:04:41 -0600

    add contact-form prototype and preparing for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://pedroalonsoms.github.io",
-  base: "/in-trend",
+  //site: "https://pedroalonsoms.github.io",
+  output: "server",
+  adapter: vercel(),
 });
---
+++ src/pages/contact-form.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from "astro";
+
+export const post: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+
+  let emailContent = "";
+  emailContent += `<p><b>Name:</b> ${formData.get("name")}</p>`;
+  emailContent += `<p><b>Company:</b> ${formData.get("company")}</p>`;
+  emailContent += `<p><b>Phone Number:</b> ${formData.get("phone_number")}</p>`;
+  emailContent += `<p><b>Email:</b> ${formData.get("email")}</p>`;
+  emailContent += `<p><b>Message:</b> ${formData.get("message")}</p>`;
+
+  const res = await fetch("https://api.sendgrid.com/v3/mail/send", {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.SEND_GRID_KEY}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      from: { email: "pedro123ben10@gmail.com" },
+      subject: "Contact Form - In-Trend's main webpage",
+      content: [
+        {
+          type: "text/html",
+          value: emailContent,
+        },
+      ],
+    }),
+  });
+
+  if (!res.ok) {
+    return new Response(null, {
+      status: 500,
+      statusText: "Email could not be delivered",
+    });
+  }
+
+  return redirect("/", 307);
+};
