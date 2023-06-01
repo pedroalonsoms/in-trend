b0408c1c569fcf0b5f5da5211a5db78f7bce5ba3
ben10pedrin <pedro123ben10@gmail.com>
2023-05-31 18:01:45 -0600

    started changing the font across the whole site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        monsterrat: ["Monsterrat", "sans-serif"],
-        "frank-ruhl-libre": ["Frank Ruhl Libre", "serif"],
+        avenir: ["Avenir", "sans-serif"],
+        "big-caslon": ["Big Caslon", "serif"],
+
+        sans: ["Avenir", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         beige: {
